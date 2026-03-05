c4c86349491a71d7b7e8b1f8c879621dfb9dbde3
hacker1happy <9547priyanshu.gupta@gmail.com>
2026-03-05 18:59:02 +0530

    master: adding readme & run file

---
+++ run_project.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Starting Backend...
+start cmd /k "cd backend && uvicorn app.main:app --reload"
+
+echo Starting Frontend...
+start cmd /k "cd frontend && npm run dev"
+
+echo Both servers are starting...
